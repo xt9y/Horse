afea2de33a18160d47de25fc27ccd8bc7f701e92
xt9y <felix@xt9y.de>
2026-09-07 15:17:26 +0200

    revert: restore pre-path-tracer raster GI renderer

--- Sources/Renderer/PathTracer/PathTracerShaders.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef RW_ENGINE_RENDERER_PATHTRACER_SHADERS_HPP
-#define RW_ENGINE_RENDERER_PATHTRACER_SHADERS_HPP
-
-#include "Renderer/PathTracer/RadianceCacheShaders.hpp"
-#include "Renderer/PathTracer/RestirShaders.hpp"
-#include "Renderer/PathTracer/SvgfShaders.hpp"
-#include "Renderer/PathTracer/WavefrontShaders.hpp"
-
-#endif
