e861242e1865e5fb3ca3fa8ab7b2dde3509e6236
xt9y <felix@xt9y.de>
2026-09-07 18:16:14 +0200

    path tracer: quarter internal resolution

--- Sources/Renderer/PathTracer/PathTracer.hpp
+++ Sources/Renderer/PathTracer/PathTracer.hpp
@@ -20,7 +20,7 @@ namespace Renderer {
 
 struct PathTracerSettings {
     bool enabled = true;
-    int resolution_divisor = 2;
+    int resolution_divisor = 4;
     int samples_per_frame = 1;
     int max_bounces = 3;
     float exposure = 1.0f;
