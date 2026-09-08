f82b79c9a162c89bef09d114f6a494198ab0af8f
xt9y <felix@xt9y.de>
2026-09-08 03:04:50 +0200

    renderer: restore portable rasterizer backend

--- Sources/Renderer/Render.hpp
+++ Sources/Renderer/Render.hpp
@@ -1,12 +1,43 @@
 #ifndef RW_ENGINE_RENDER_HPP
 #define RW_ENGINE_RENDER_HPP
 
+#include "Ecs/Ecs.hpp"
 #include "Renderer/Components.hpp"
 #include "Renderer/PathTracer/PathTracer.hpp"
 
+#include <cstdint>
+#include <unordered_map>
+
 namespace Renderer {
 
-using Rasterizer = PathTracer;
+class Rasterizer {
+public:
+    bool init();
+    void resize(int width, int height);
+    void render(const Ecs::World& world);
+    void shutdown();
+
+    bool initialized() const { return initialized_; }
+    bool enabled() const { return path_tracer_.enabled(); }
+    void setEnabled(bool enabled) { path_tracer_.setEnabled(enabled); }
+
+    PathTracerSettings& settings() { return path_tracer_.settings(); }
+    const PathTracerSettings& settings() const { return path_tracer_.settings(); }
+
+    bool usingPathTracer() const { return use_path_tracer_; }
+
+private:
+    bool initCompatibility();
+    void renderCompatibility(const Ecs::World& world);
+    unsigned int textureFor(std::uint32_t handle);
+
+    PathTracer path_tracer_{};
+    bool initialized_ = false;
+    bool use_path_tracer_ = false;
+    int width_ = 1;
+    int height_ = 1;
+    std::unordered_map<std::uint32_t, unsigned int> textures_;
+};
 
 } // namespace Renderer
 
