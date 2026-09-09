b92d2ca65c349de14606c6e9aa80a8875409e4f0
xt9y <felix@xt9y.de>
2026-09-10 00:13:03 +0200

    Test ray tracer fast default

--- tests/renderer_systems_contract.cpp
+++ tests/renderer_systems_contract.cpp
@@ -74,6 +74,7 @@ int main()
 
     assert(raytracer_header.find("class RayTracer final : public IRenderer") != std::string::npos);
     assert(raytracer_header.find("struct RayTracerSettings") != std::string::npos);
+    assert(raytracer_header.find("int resolution_divisor = 4;") != std::string::npos);
     assert(render.find("Renderer/RayTracer/RayTracer.hpp") != std::string::npos);
 
     assert(build.find("Sources/Renderer/Systems/Scene.cpp") != std::string::npos);
