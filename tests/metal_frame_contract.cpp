ac42b042ce7ae513aec08004b3e8fbe1e87849cd
xt9y <felix@xt9y.de>
2026-09-09 20:01:39 +0200

    test: follow expanded Metal texture bindings

--- tests/metal_frame_contract.cpp
+++ tests/metal_frame_contract.cpp
@@ -13,7 +13,7 @@ int main()
     assert(source.find("bool PathTracer::renderScene") != std::string::npos);
     assert(source.find("void PathTracer::present") != std::string::npos);
     assert(source.find("dispatchAndCompose") != std::string::npos);
-    assert(source.find("Metal.setTexture(command, primary_depth, 17u)") != std::string::npos);
+    assert(source.find("Metal.setTexture(command, primary_depth, 33u)") != std::string::npos);
     assert(source.find("output.depth = Internal::DepthSource::LinearTexture") != std::string::npos);
     return 0;
 }
