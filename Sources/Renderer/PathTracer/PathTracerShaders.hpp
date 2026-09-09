d0688539b363b9c4f1d19443d82b0d09e1f1069b
xt9y <felix@xt9y.de>
2026-09-09 15:13:02 +0200

    feat: route OpenGL path tracing through shared GI shader

--- Sources/Renderer/PathTracer/PathTracerShaders.hpp
+++ Sources/Renderer/PathTracer/PathTracerShaders.hpp
@@ -1,6 +1,10 @@
 #ifndef RW_ENGINE_RENDERER_PATHTRACER_SHADERS_HPP
 #define RW_ENGINE_RENDERER_PATHTRACER_SHADERS_HPP
 
+#define trace legacy_trace
 #include "Renderer/PathTracer/PathTracerWorldFastShaders.hpp"
+#undef trace
+
+#include "Renderer/PathTracer/PathTracerSharedGITrace.hpp"
 
 #endif
