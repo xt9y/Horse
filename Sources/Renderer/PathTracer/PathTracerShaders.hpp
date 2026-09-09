e88a17e1c65ce43570d176774239369f17fbc3d3
xt9y <felix@xt9y.de>
2026-09-09 19:12:50 +0200

    fix: use reconstructed OpenGL presentation

--- Sources/Renderer/PathTracer/PathTracerShaders.hpp
+++ Sources/Renderer/PathTracer/PathTracerShaders.hpp
@@ -2,9 +2,14 @@
 #define RW_ENGINE_RENDERER_PATHTRACER_SHADERS_HPP
 
 #define trace legacy_trace
+#define present_vertex legacy_present_vertex
+#define present_fragment legacy_present_fragment
 #include "Renderer/PathTracer/PathTracerWorldFastShaders.hpp"
+#undef present_fragment
+#undef present_vertex
 #undef trace
 
 #include "Renderer/PathTracer/PathTracerSharedGITrace.hpp"
+#include "Renderer/PathTracer/PathTracerPresentShaders.hpp"
 
 #endif
