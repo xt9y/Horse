a511eb729967fec20831cb6bfdc2538a847858e0
xt9y <felix@xt9y.de>
2026-09-09 11:59:44 +0200

    feat: composite fonts after scene rendering

--- Sources/Renderer/Renderer.cpp
+++ Sources/Renderer/Renderer.cpp
@@ -1,11 +1,14 @@
 #include "Renderer/Renderer.hpp"
 
+#include "Renderer/FontPass.hpp"
+
 namespace Renderer {
 
 void IRenderer::render(const Ecs::World& world)
 {
     Internal::FrameOutput output;
     if (!renderScene(world, output)) return;
+    Internal::renderFonts(world, output);
     present(output);
 }
 
