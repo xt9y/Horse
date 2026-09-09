05c561d35d8575ec663d9031b9d6a77f9cb160a2
xt9y <felix@xt9y.de>
2026-09-09 15:07:54 +0200

    feat: define OpenGL GI consumer adapter

---
+++ Sources/Renderer/GlobalIlluminationOpenGL.hpp
@@ -0,0 +1,16 @@
+#ifndef RW_ENGINE_RENDERER_GLOBAL_ILLUMINATION_OPENGL_HPP
+#define RW_ENGINE_RENDERER_GLOBAL_ILLUMINATION_OPENGL_HPP
+
+#include "Renderer/GlobalIllumination.hpp"
+
+namespace Renderer::Internal {
+
+void bindGlobalIlluminationOpenGL(
+    unsigned int program,
+    const GlobalIllumination::Field *field
+);
+void shutdownGlobalIlluminationOpenGL();
+
+} // namespace Renderer::Internal
+
+#endif
