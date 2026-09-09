7238dd36b5228518b64af476c5a90d033e3e1626
xt9y <felix@xt9y.de>
2026-09-09 21:19:30 +0200

    test: require high quality rasterizer shadows

---
+++ tests/rasterizer_shadow_quality_contract.cpp
@@ -0,0 +1,31 @@
+#include <cassert>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+static std::string read(const char *path)
+{
+    std::ifstream file(path);
+    assert(file.good());
+    std::ostringstream out;
+    out << file.rdbuf();
+    return out.str();
+}
+
+int main()
+{
+    const std::string source = read("Sources/Renderer/Rasterizer/Rasterizer.cpp");
+    const std::string shaders = read("Sources/Renderer/Rasterizer/RasterizerShaders.hpp");
+
+    assert(source.find("constexpr int kShadowResolution = 2048;") != std::string::npos);
+    assert(source.find("shadow_framebuffer") != std::string::npos);
+    assert(source.find("shadow_depth_renderbuffer") != std::string::npos);
+    assert(source.find("glFramebufferTexture2D") != std::string::npos);
+    assert(source.find("glRenderbufferStorage") != std::string::npos);
+    assert(source.find("const int requested_size = kShadowResolution;") != std::string::npos);
+
+    assert(shaders.find("for (int y = -2; y <= 2; ++y)") != std::string::npos);
+    assert(shaders.find("for (int x = -2; x <= 2; ++x)") != std::string::npos);
+    assert(shaders.find("visibility / 25.0") != std::string::npos);
+    return 0;
+}
