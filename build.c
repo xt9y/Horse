f81fdf45766ff0859e4ef59e639e5713d8399fc8
xt9y <felix@xt9y.de>
2026-09-09 12:27:07 +0200

    fix: build Metal font frame path

--- build.c
+++ build.c
@@ -45,6 +45,19 @@ static void configureLibrary(C_Target *target)
 #endif
 }
 
+static void configureContract(C_Target *target)
+{
+    c_include(target, ".");
+    c_include(target, "Sources");
+    c_flag(target, "-std=c++20");
+    c_warnings_strict(target);
+#ifdef __APPLE__
+    c_link_system(target, "c++");
+#else
+    c_link_system(target, "stdc++");
+#endif
+}
+
 void build(C_Build *b)
 {
     C_Target *library = c_shared_library(b, "Horse");
@@ -54,12 +67,21 @@ void build(C_Build *b)
     c_sources(library, "Sources/Models/*/*.cpp");
     c_sources(library, "Sources/Renderer/Rasterizer/Rasterizer.cpp");
 #ifdef __APPLE__
-    c_sources(library, "Sources/Renderer/PathTracer/PathTracerMetalFrame.cpp");
+    c_sources(library, "Sources/Renderer/PathTracer/PathTracerMetal.cpp");
 #else
     c_sources(library, "Sources/Renderer/PathTracer/PathTracer.cpp");
 #endif
 
     c_flag(library, "-std=c++20");
     configureLibrary(library);
+
+    C_Target *pathtracer_depth_contract = c_test(b, "pathtracer-depth-contract");
+    c_sources(pathtracer_depth_contract, "tests/pathtracer_depth_contract.cpp");
+    configureContract(pathtracer_depth_contract);
+
+    C_Target *metal_frame_contract = c_test(b, "metal-frame-contract");
+    c_sources(metal_frame_contract, "tests/metal_frame_contract.cpp");
+    configureContract(metal_frame_contract);
+
     c_default_target(b, library);
 }
