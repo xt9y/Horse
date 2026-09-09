9db28e84af255326a2563f26a534f1fa311d87f1
xt9y <felix@xt9y.de>
2026-09-10 00:59:31 +0200

    test: define renderer layout and photon mapping contract

--- tests/renderer_systems_contract.cpp
+++ tests/renderer_systems_contract.cpp
@@ -21,37 +21,38 @@ static bool exists(const char *path)
 int main()
 {
     const std::string camera = read("Sources/Camera.cpp");
-    const std::string scene = read("Sources/Renderer/Systems/Scene.hpp");
-    const std::string scene_compat = read("Sources/Renderer/Scene.hpp");
-    const std::string cache = read("Sources/Renderer/Systems/SceneCache.hpp");
+    const std::string components = read("Sources/Renderer/Components.hpp");
+    const std::string scene = read("Sources/Renderer/Scenes/Scene.hpp");
+    const std::string cache = read("Sources/Renderer/Scenes/SceneCache.hpp");
     const std::string progressive = read("Sources/Renderer/Systems/ProgressiveState.hpp");
     const std::string uniforms = read("Sources/Renderer/Systems/Uniforms.hpp");
     const std::string program = read("Sources/Renderer/Systems/OpenGL/Program.hpp");
     const std::string textures = read("Sources/Renderer/Systems/OpenGL/TextureCache.hpp");
-    const std::string gl_resources = read("Sources/Renderer/Systems/OpenGLSceneResources.cpp");
-    const std::string metal_resources = read("Sources/Renderer/Systems/MetalSceneResources.hpp");
-    const std::string gi_gl = read("Sources/Renderer/GlobalIlluminationOpenGL.cpp");
-    const std::string gi_metal = read("Sources/Renderer/GlobalIlluminationMetal.cpp");
-    const std::string rasterizer = read("Sources/Renderer/Rasterizer/Rasterizer.cpp");
-    const std::string pathtracer = read("Sources/Renderer/PathTracer/PathTracer.cpp");
-    const std::string pathtracer_metal = read("Sources/Renderer/PathTracer/PathTracerMetal.cpp");
-    const std::string raytracer = read("Sources/Renderer/RayTracer/RayTracer.cpp");
-    const std::string raytracer_metal = read("Sources/Renderer/RayTracer/RayTracerMetal.cpp");
-    const std::string raytracer_metal_shaders = read("Sources/Renderer/RayTracer/RayTracerMetalShaders.hpp");
+    const std::string gl_resources = read("Sources/Renderer/Scenes/OpenGL/SceneResources.cpp");
+    const std::string metal_resources = read("Sources/Renderer/Scenes/Metal/SceneResources.hpp");
+    const std::string gi = read("Sources/Renderer/GlobalIllumination/GlobalIllumination.cpp");
+    const std::string gi_gl = read("Sources/Renderer/GlobalIllumination/OpenGL/GlobalIlluminationOpenGL.cpp");
+    const std::string gi_metal = read("Sources/Renderer/GlobalIllumination/Metal/GlobalIlluminationMetal.cpp");
+    const std::string photon_header = read("Sources/Renderer/GlobalIllumination/PhotonMapping/PhotonMap.hpp");
+    const std::string photon_source = read("Sources/Renderer/GlobalIllumination/PhotonMapping/PhotonMap.cpp");
+    const std::string trace_scene = read("Sources/Renderer/GlobalIllumination/TraceScene.hpp");
+    const std::string font_pass = read("Sources/Renderer/Fonts/FontPass.hpp");
+    const std::string font_gl = read("Sources/Renderer/Fonts/OpenGL/FontPassOpenGL.cpp");
+    const std::string font_metal = read("Sources/Renderer/Fonts/Metal/FontPassMetal.cpp");
+    const std::string rasterizer = read("Sources/Renderer/Rasterizer/OpenGL/RasterizerOpenGL.cpp");
+    const std::string pathtracer = read("Sources/Renderer/PathTracer/OpenGL/PathTracerOpenGL.cpp");
+    const std::string pathtracer_metal = read("Sources/Renderer/PathTracer/Metal/PathTracerMetal.cpp");
+    const std::string raytracer = read("Sources/Renderer/RayTracer/OpenGL/RayTracerOpenGL.cpp");
+    const std::string raytracer_metal = read("Sources/Renderer/RayTracer/Metal/RayTracerMetal.cpp");
+    const std::string raytracer_metal_shaders = read("Sources/Renderer/RayTracer/Metal/RayTracerMetalShaders.hpp");
     const std::string raytracer_header = read("Sources/Renderer/RayTracer/RayTracer.hpp");
     const std::string render = read("Sources/Renderer/Render.hpp");
     const std::string build = read("build.c");
 
     assert(camera.find("world.markChanged()") == std::string::npos);
-    assert(gi_gl.find("uploaded_revision") != std::string::npos);
-    assert(gi_gl.find("revision == uploaded_revision") != std::string::npos);
-    assert(gi_metal.find("uploaded_revision") != std::string::npos);
-    assert(gi_metal.find("revision == uploaded_revision") != std::string::npos);
 
-    assert(scene.find("namespace Renderer::Systems::Scene") != std::string::npos);
-    assert(scene_compat.find("namespace Scene = Systems::Scene") != std::string::npos);
-
-    assert(cache.find("namespace Renderer::Systems") != std::string::npos);
+    assert(scene.find("namespace Renderer::Scenes") != std::string::npos);
+    assert(cache.find("namespace Renderer::Scenes") != std::string::npos);
     assert(cache.find("class SceneCache") != std::string::npos);
     assert(cache.find("struct GpuNode") != std::string::npos);
     assert(cache.find("struct GpuTriangle") != std::string::npos);
@@ -61,34 +62,59 @@ int main()
 
     assert(program.find("namespace Renderer::Systems::OpenGL") != std::string::npos);
     assert(program.find("class Program") != std::string::npos);
-    assert(program.find("bool createGraphics") != std::string::npos);
-    assert(program.find("bool createCompute") != std::string::npos);
     assert(textures.find("class TextureCache") != std::string::npos);
-    assert(gl_resources.find("Systems/OpenGL/TextureCache.hpp") != std::string::npos);
-    assert(metal_resources.find("class MetalSceneResources") != std::string::npos);
+    assert(gl_resources.find("namespace Renderer::Scenes::OpenGL") != std::string::npos);
+    assert(metal_resources.find("namespace Renderer::Scenes::Metal") != std::string::npos);
+    assert(metal_resources.find("class SceneResources") != std::string::npos);
 
-    assert(rasterizer.find("Renderer/Systems/Scene.hpp") != std::string::npos);
-    assert(rasterizer.find("Renderer/Systems/OpenGL/Program.hpp") != std::string::npos);
-    assert(rasterizer.find("Renderer/Systems/OpenGL/TextureCache.hpp") != std::string::npos);
-    assert(pathtracer.find("Renderer/Systems/") != std::string::npos);
-    assert(pathtracer_metal.find("Renderer/Systems/") != std::string::npos);
-    assert(raytracer.find("Renderer/Systems/") != std::string::npos);
-    assert(raytracer_metal.find("Renderer/Systems/") != std::string::npos);
+    assert(gi.find("PhotonMapping::PhotonMap") != std::string::npos);
+    assert(gi.find("photon_map.rebuild") != std::string::npos);
+    assert(gi.find("photon_map.sample") != std::string::npos);
+    assert(gi_gl.find("uploaded_revision") != std::string::npos);
+    assert(gi_gl.find("revision == uploaded_revision") != std::string::npos);
+    assert(gi_metal.find("uploaded_revision") != std::string::npos);
+    assert(gi_metal.find("revision == uploaded_revision") != std::string::npos);
 
-    assert(rasterizer.find("Renderer/RayTracing/") == std::string::npos);
-    assert(pathtracer.find("Renderer/RayTracing/") == std::string::npos);
-    assert(pathtracer_metal.find("Renderer/RayTracing/") == std::string::npos);
-    assert(raytracer.find("Renderer/RayTracing/") == std::string::npos);
-    assert(raytracer_metal.find("Renderer/RayTracing/") == std::string::npos);
-    assert(!exists("Sources/Renderer/RayTracing/Compatibility.hpp"));
+    assert(trace_scene.find("class TraceScene") != std::string::npos);
+    assert(trace_scene.find("const Scenes::SceneCache& cache() const") != std::string::npos);
+    assert(photon_header.find("class PhotonMap") != std::string::npos);
+    assert(photon_header.find("struct Settings") != std::string::npos);
+    assert(photon_header.find("void rebuild") != std::string::npos);
+    assert(photon_header.find("Vec3 sample") != std::string::npos);
+    assert(photon_source.find("std::unordered_map<Cell") != std::string::npos);
+    assert(photon_source.find("cosineHemisphere") != std::string::npos);
+    assert(components.find("bool photon_mapping = true;") != std::string::npos);
+    assert(components.find("std::uint32_t photon_count = 4096u;") != std::string::npos);
+    assert(components.find("float photon_radius = 0.0f;") != std::string::npos);
+
+    assert(font_pass.find("namespace Renderer::Internal") != std::string::npos);
+    assert(font_gl.find("Renderer/Fonts/FontPass.hpp") != std::string::npos);
+    assert(font_metal.find("Renderer/Fonts/FontPass.hpp") != std::string::npos);
+
+    assert(rasterizer.find("Renderer/Scenes/Scene.hpp") != std::string::npos);
+    assert(pathtracer.find("Renderer/Scenes/") != std::string::npos);
+    assert(pathtracer_metal.find("Renderer/Scenes/") != std::string::npos);
+    assert(raytracer.find("Renderer/Scenes/") != std::string::npos);
+    assert(raytracer_metal.find("Renderer/Scenes/") != std::string::npos);
+
+    assert(!exists("Sources/Renderer/FontAtlas.cpp"));
+    assert(!exists("Sources/Renderer/FontPassMetal.cpp"));
+    assert(!exists("Sources/Renderer/GlobalIllumination.cpp"));
+    assert(!exists("Sources/Renderer/GlobalIlluminationMetal.cpp"));
+    assert(!exists("Sources/Renderer/Scene.hpp"));
+    assert(!exists("Sources/Renderer/Systems/Scene.cpp"));
+    assert(!exists("Sources/Renderer/Systems/MetalSceneResources.cpp"));
+    assert(!exists("Sources/Renderer/Systems/OpenGLSceneResources.cpp"));
+    assert(!exists("Sources/Renderer/PathTracer/PathTracer.cpp"));
+    assert(!exists("Sources/Renderer/PathTracer/PathTracerMetal.cpp"));
+    assert(!exists("Sources/Renderer/RayTracer/RayTracer.cpp"));
+    assert(!exists("Sources/Renderer/RayTracer/RayTracerMetal.cpp"));
 
     assert(raytracer_header.find("class RayTracer final : public IRenderer") != std::string::npos);
-    assert(raytracer_header.find("struct RayTracerSettings") != std::string::npos);
     assert(raytracer_header.find("int resolution_divisor = 4;") != std::string::npos);
     assert(raytracer.find("std::clamp(settings.resolution_divisor, 4, 8)") != std::string::npos);
     assert(raytracer_metal.find("std::clamp(settings.resolution_divisor, 4, 8)") != std::string::npos);
-    assert(raytracer_metal.find("Renderer/RayTracer/RayTracerMetalShaders.hpp") != std::string::npos);
-    assert(raytracer_metal.find("Renderer/PathTracer/PathTracerMetalShaders.hpp") == std::string::npos);
+    assert(raytracer_metal.find("Renderer/RayTracer/Metal/RayTracerMetalShaders.hpp") != std::string::npos);
     assert(raytracer_metal.find("Metal.createTriangleAccelerationStructure") != std::string::npos);
     assert(raytracer_metal.find("Metal.setAccelerationStructure") != std::string::npos);
     assert(raytracer_metal.find("meaningful_alpha") != std::string::npos);
@@ -103,10 +129,13 @@ int main()
     assert(raytracer_metal_shaders.find("deterministicDepthAlpha") == std::string::npos);
     assert(render.find("Renderer/RayTracer/RayTracer.hpp") != std::string::npos);
 
-    assert(build.find("Sources/Renderer/Systems/Scene.cpp") != std::string::npos);
-    assert(build.find("Sources/Renderer/Systems/SceneCache.cpp") != std::string::npos);
-    assert(build.find("Sources/Renderer/Systems/OpenGL/Program.cpp") != std::string::npos);
-    assert(build.find("Sources/Renderer/Systems/OpenGL/TextureCache.cpp") != std::string::npos);
+    assert(build.find("Sources/Renderer/Fonts/FontPass.cpp") != std::string::npos);
+    assert(build.find("Sources/Renderer/GlobalIllumination/GlobalIllumination.cpp") != std::string::npos);
+    assert(build.find("Sources/Renderer/GlobalIllumination/PhotonMapping/PhotonMap.cpp") != std::string::npos);
+    assert(build.find("Sources/Renderer/Scenes/Scene.cpp") != std::string::npos);
+    assert(build.find("Sources/Renderer/Scenes/SceneCache.cpp") != std::string::npos);
+    assert(build.find("Sources/Renderer/PathTracer/OpenGL/PathTracerOpenGL.cpp") != std::string::npos);
+    assert(build.find("Sources/Renderer/PathTracer/Metal/PathTracerMetal.cpp") != std::string::npos);
     assert(build.find("renderer-systems-contract") != std::string::npos);
     return 0;
 }
