012a1786650c867f417e86463cf3d6dc3c9ab823
xt9y <felix@xt9y.de>
2026-09-09 17:28:02 +0200

    test: keep alpha-cutout contract material-neutral

--- tests/pathtracer_alpha_cutout_contract.cpp
+++ tests/pathtracer_alpha_cutout_contract.cpp
@@ -16,19 +16,15 @@ int main()
 {
     const std::string gl = read("Sources/Renderer/PathTracer/PathTracerSharedGITrace.hpp");
     const std::string metal = read("Sources/Renderer/PathTracer/PathTracerMetalShaders.hpp");
-    const std::string gl_upload = read("Sources/Renderer/PathTracer/PathTracer.cpp");
-    const std::string metal_upload = read("Sources/Renderer/PathTracer/PathTracerMetal.cpp");
 
     assert(gl.find("alphaCutoutPass") != std::string::npos);
-    assert(gl.find("material.data.y") != std::string::npos);
+    assert(gl.find("material.base_color.a") != std::string::npos);
+    assert(gl.find("texel.a") != std::string::npos);
     assert(gl.find("if (!alphaCutoutPass") != std::string::npos);
-    assert(metal.find("alphaCutoutPass") != std::string::npos);
-    assert(metal.find("material.data.y") != std::string::npos);
-    assert(metal.find("if (!alphaCutoutPass") != std::string::npos);
 
-    assert(gl_upload.find("meaningful_alpha") != std::string::npos);
-    assert(gl_upload.find("gpu_material.data[1]") != std::string::npos);
-    assert(metal_upload.find("meaningful_alpha") != std::string::npos);
-    assert(metal_upload.find("gpu_material.data[1]") != std::string::npos);
+    assert(metal.find("alphaCutoutPass") != std::string::npos);
+    assert(metal.find("material.base_color.a") != std::string::npos);
+    assert(metal.find("texel.a") != std::string::npos);
+    assert(metal.find("if (!alphaCutoutPass") != std::string::npos);
     return 0;
 }
