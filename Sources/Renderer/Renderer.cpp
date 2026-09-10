111ba18c8fdebb2e5298d2273f52218964363c46
xt9y <felix@xt9y.de>
2026-09-10 12:26:21 +0200

    renderer: run debug pass before fonts and UI

--- Sources/Renderer/Renderer.cpp
+++ Sources/Renderer/Renderer.cpp
@@ -1,5 +1,6 @@
 #include "Renderer/Renderer.hpp"
 
+#include "Renderer/Debug/Internal.hpp"
 #include "Renderer/Fonts/FontPass.hpp"
 #include "Renderer/GlobalIllumination/GlobalIllumination.hpp"
 #include "UI/Internal.hpp"
@@ -13,8 +14,9 @@ void IRenderer::render(const Ecs::World& world)
     Internal::FrameOutput output;
     output.global_illumination = GlobalIllumination::update(world);
     if (!renderScene(world, output)) return;
+    Debug::Internal::render(world, output);
     Internal::renderFonts(world, output);
-    UI::Internal::render(world, output);
+    UI::Internal::render(output);
     present(output);
     if (output.api == Internal::GraphicsApi::OpenGL) {
         Display.updateNoMessages();
