2cd2b88a7671530c481ce05cdbb440136a56a1bb
xt9y <felix@xt9y.de>
2026-09-10 17:31:47 +0200

    debug: reduce API to BVH and frozen viewport inspection

--- Sources/Renderer/Debug/Debug.hpp
+++ Sources/Renderer/Debug/Debug.hpp
@@ -1,22 +1,66 @@
 #ifndef HORSE_RENDERER_DEBUG_DEBUG_HPP
 #define HORSE_RENDERER_DEBUG_DEBUG_HPP
 
+#include "Ecs/Ecs.hpp"
+#include "Renderer/Components.hpp"
+
+#include <cstddef>
+
 namespace Renderer::Debug {
 
-struct Settings {
-    bool wireframe = false;
-    bool show_bvh = false;
-    bool show_photons = false;
-    bool show_gi_probes = false;
-    bool show_scene_bounds = false;
-    bool show_light = false;
-    bool show_normals = false;
-    int bvh_level = 2;
-    float overlay_opacity = 0.80f;
-    float photon_size = 2.0f;
+struct SnapshotInfo {
+    bool frozen = false;
+    std::size_t visible_entities = 0u;
+    std::size_t culled_entities = 0u;
+    std::size_t visible_triangles = 0u;
+    std::size_t culled_triangles = 0u;
+    Ecs::Entity player_camera = Ecs::INVALID_ENTITY;
+    Ecs::Entity debug_camera = Ecs::INVALID_ENTITY;
 };
 
-Settings& settings();
+class Inspector {
+public:
+    Inspector();
+    ~Inspector();
+
+    Inspector(const Inspector&) = delete;
+    Inspector& operator=(const Inspector&) = delete;
+
+    void setShowBvh(bool value);
+    void setShowViewport(bool value);
+    void setBvhLevel(int value);
+    void setOverlayOpacity(float value);
+    void setBvhColor(Vec4 value);
+    void setHighlightColor(Vec4 value);
+    void setPlayerColor(Vec4 value);
+    void setPlayerHeight(float value);
+    void setCameraMarkerSize(float value);
+
+    bool showBvh() const;
+    bool showViewport() const;
+    int bvhLevel() const;
+    float overlayOpacity() const;
+
+    bool freeze(Ecs::World& world, int width, int height);
+    void unfreeze(Ecs::World& world);
+    bool frozen() const;
+    Ecs::Entity debugCamera() const;
+    SnapshotInfo snapshotInfo() const;
+
+    void clear(Ecs::World *world = nullptr);
+
+private:
+    struct Impl;
+    Impl *impl_ = nullptr;
+
+    friend void renderInspector(
+        Inspector& inspector,
+        const Ecs::World& world,
+        void *frame_output
+    );
+};
+
+Inspector& inspector();
 void clear();
 void shutdown();
 
