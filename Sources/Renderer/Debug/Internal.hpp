e8374f410ac16f0d9868648eaf27a8f7c5fd5191
xt9y <felix@xt9y.de>
2026-09-10 12:23:16 +0200

    renderer: define debug pass internals

---
+++ Sources/Renderer/Debug/Internal.hpp
@@ -0,0 +1,44 @@
+#ifndef HORSE_RENDERER_DEBUG_INTERNAL_HPP
+#define HORSE_RENDERER_DEBUG_INTERNAL_HPP
+
+#include "Renderer/Math.hpp"
+#include "Renderer/Renderer.hpp"
+
+#include <array>
+#include <cstdint>
+#include <vector>
+
+namespace Renderer::Debug::Internal {
+
+struct Vertex {
+    std::array<float, 4> position{};
+    std::array<float, 4> color{};
+};
+
+void render(const Ecs::World& world, Renderer::Internal::FrameOutput& output);
+
+void renderOpenGL(
+    const std::vector<Vertex>& wireframe,
+    std::uint64_t wireframe_revision,
+    const std::vector<Vertex>& dynamic,
+    const Math::Mat4& projection,
+    const Math::Mat4& view,
+    Renderer::Internal::FrameOutput& output
+);
+void shutdownOpenGL();
+
+#ifdef __APPLE__
+void renderMetal(
+    const std::vector<Vertex>& wireframe,
+    std::uint64_t wireframe_revision,
+    const std::vector<Vertex>& dynamic,
+    const Math::Mat4& projection,
+    const Math::Mat4& view,
+    Renderer::Internal::FrameOutput& output
+);
+void shutdownMetal();
+#endif
+
+} // namespace Renderer::Debug::Internal
+
+#endif
