a10965046c596a865bdf3f4f91d84486873f2275
felixjaschul <felix@jaschul.de>
2026-09-10 19:47:59 +0200

    pathtracer: remove obsolete shader variants

--- Sources/Renderer/PathTracer/OpenGL/PathTracerShaders.hpp
+++ Sources/Renderer/PathTracer/OpenGL/PathTracerShaders.hpp
@@ -1,12 +1,5 @@
 #ifndef RW_ENGINE_RENDERER_PATHTRACER_OPENGL_SHADERS_HPP
 #define RW_ENGINE_RENDERER_PATHTRACER_OPENGL_SHADERS_HPP
-#define trace legacy_trace
-#define present_vertex legacy_present_vertex
-#define present_fragment legacy_present_fragment
-#include "Renderer/PathTracer/OpenGL/PathTracerWorldFastShaders.hpp"
-#undef present_fragment
-#undef present_vertex
-#undef trace
 #include "Renderer/PathTracer/OpenGL/PathTracerSharedGITrace.hpp"
 #include "Renderer/PathTracer/OpenGL/PathTracerPresentShaders.hpp"
 #endif
