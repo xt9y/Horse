0206057996d811923036527d458a2f4e160f13c2
xt9y <felix@xt9y.de>
2026-09-10 02:44:43 +0200

    ui: render ImGui as shared post pass

--- Sources/Renderer/Renderer.cpp
+++ Sources/Renderer/Renderer.cpp
@@ -2,6 +2,7 @@
 
 #include "Renderer/Fonts/FontPass.hpp"
 #include "Renderer/GlobalIllumination/GlobalIllumination.hpp"
+#include "UI/Internal.hpp"
 
 #include <lwcgl/lwcgl.h>
 
@@ -13,6 +14,7 @@ void IRenderer::render(const Ecs::World& world)
     output.global_illumination = GlobalIllumination::update(world);
     if (!renderScene(world, output)) return;
     Internal::renderFonts(world, output);
+    UI::Internal::render(output);
     present(output);
     if (output.api == Internal::GraphicsApi::OpenGL) {
         Display.updateNoMessages();
