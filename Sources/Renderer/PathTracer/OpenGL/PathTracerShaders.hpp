97b72a0d23c00c5de5574d202958cca584a0e74f
xt9y <felix@xt9y.de>
2026-09-11 01:12:20 +0200

    raytracing: consume shared pbr materials

--- Sources/Renderer/PathTracer/OpenGL/PathTracerShaders.hpp
+++ Sources/Renderer/PathTracer/OpenGL/PathTracerShaders.hpp
@@ -1,5 +1,5 @@
 #ifndef RW_ENGINE_RENDERER_PATHTRACER_OPENGL_SHADERS_HPP
 #define RW_ENGINE_RENDERER_PATHTRACER_OPENGL_SHADERS_HPP
-#include "Renderer/PathTracer/OpenGL/PathTracerSharedGITrace.hpp"
+#include "Renderer/PathTracer/OpenGL/PbrTraceShaders.hpp"
 #include "Renderer/PathTracer/OpenGL/PathTracerPresentShaders.hpp"
 #endif
