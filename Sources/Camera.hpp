bf54ef9ccce9b6ff2584dcf88ef117b0f5af3279
xt9y <felix@xt9y.de>
2026-09-10 17:24:22 +0200

    camera: expose runtime tuning

--- Sources/Camera.hpp
+++ Sources/Camera.hpp
@@ -7,9 +7,9 @@
 namespace Camera {
 
 struct CameraComponent {
-    float fov_degrees = 60.0f;
-    float near_plane = 0.1f;
-    bool active = true;
+    float fov_degrees = 0.0f;
+    float near_plane = 0.0f;
+    bool active = false;
 };
 
 Ecs::Entity activeCamera(const Ecs::World& world);
@@ -18,9 +18,29 @@ Renderer::Vec3 strafeDirection(float yaw_degrees);
 
 class Controller {
 public:
+    void setSpeed(float value) { speed_ = value; }
+    void setSprintMultiplier(float value) { sprint_multiplier_ = value; }
+    void setMouseSensitivity(float value) { mouse_sensitivity_ = value; }
+    void setPitchRange(float minimum, float maximum)
+    {
+        minimum_pitch_ = minimum;
+        maximum_pitch_ = maximum;
+    }
+
+    float speed() const { return speed_; }
+    float sprintMultiplier() const { return sprint_multiplier_; }
+    float mouseSensitivity() const { return mouse_sensitivity_; }
+    float minimumPitch() const { return minimum_pitch_; }
+    float maximumPitch() const { return maximum_pitch_; }
+
     void update(Ecs::World& world, float delta_seconds);
 
 private:
+    float speed_ = 0.0f;
+    float sprint_multiplier_ = 0.0f;
+    float mouse_sensitivity_ = 0.0f;
+    float minimum_pitch_ = 0.0f;
+    float maximum_pitch_ = 0.0f;
     bool mouse_initialized_ = false;
 };
 
