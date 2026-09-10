4dd24c7bc3f261652b8ca9dc430de329e749154e
xt9y <felix@xt9y.de>
2026-09-10 02:44:19 +0200

    ui: add OpenGL ImGui backend

---
+++ Sources/UI/OpenGL/UIOpenGL.cpp
@@ -0,0 +1,43 @@
+#include "UI/Internal.hpp"
+
+#include <imgui.h>
+
+#ifdef __APPLE__
+#include <backends/imgui_impl_opengl2.h>
+#else
+#include <backends/imgui_impl_opengl3.h>
+#endif
+
+namespace UI::Internal {
+
+bool initOpenGL()
+{
+#ifdef __APPLE__
+    return ImGui_ImplOpenGL2_Init();
+#else
+    return ImGui_ImplOpenGL3_Init("#version 430");
+#endif
+}
+
+void shutdownOpenGL()
+{
+#ifdef __APPLE__
+    ImGui_ImplOpenGL2_Shutdown();
+#else
+    ImGui_ImplOpenGL3_Shutdown();
+#endif
+}
+
+void renderOpenGL(ImDrawData *draw_data)
+{
+    if (!draw_data) return;
+#ifdef __APPLE__
+    ImGui_ImplOpenGL2_NewFrame();
+    ImGui_ImplOpenGL2_RenderDrawData(draw_data);
+#else
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplOpenGL3_RenderDrawData(draw_data);
+#endif
+}
+
+} // namespace UI::Internal
