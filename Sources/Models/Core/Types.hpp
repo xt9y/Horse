c6a1063e2f1804c944e9f2cf85c8214f52dace40
xt9y <felix@xt9y.de>
2026-09-11 10:38:45 +0200

    models: expand generic asset math types

--- Sources/Models/Core/Types.hpp
+++ Sources/Models/Core/Types.hpp
@@ -1,6 +1,8 @@
 #ifndef RW_ENGINE_MODELS_CORE_TYPES_HPP
 #define RW_ENGINE_MODELS_CORE_TYPES_HPP
 
+#include <array>
+
 namespace Models {
 
 struct Vec2 {
@@ -13,6 +15,32 @@ struct Vec3 {
     float y = 0.0f;
     float z = 0.0f;
 };
+
+struct Vec4 {
+    float x = 0.0f;
+    float y = 0.0f;
+    float z = 0.0f;
+    float w = 0.0f;
+};
+
+struct Quat {
+    float x = 0.0f;
+    float y = 0.0f;
+    float z = 0.0f;
+    float w = 1.0f;
+};
+
+using Mat4 = std::array<float, 16>;
+
+inline Mat4 identityMatrix()
+{
+    return {
+        1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 1.0f,
+    };
+}
 
 } // namespace Models
 
