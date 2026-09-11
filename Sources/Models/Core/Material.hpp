5dec42154ea450d7a09cb2faaa35225ad27240f9
xt9y <felix@xt9y.de>
2026-09-11 10:39:03 +0200

    models: retain full pbr texture metadata

--- Sources/Models/Core/Material.hpp
+++ Sources/Models/Core/Material.hpp
@@ -16,6 +16,28 @@ enum class AlphaMode : std::uint8_t {
     Blend,
 };
 
+struct SamplerData {
+    int mag_filter = 9729;
+    int min_filter = 9987;
+    int wrap_s = 10497;
+    int wrap_t = 10497;
+};
+
+struct TextureTransform {
+    Vec2 offset {0.0f, 0.0f};
+    Vec2 scale {1.0f, 1.0f};
+    float rotation = 0.0f;
+    int texcoord = -1;
+};
+
+struct TextureInfo {
+    TextureHandle texture = INVALID_TEXTURE;
+    std::uint32_t texcoord = 0u;
+    TextureTransform transform{};
+    SamplerData sampler{};
+    float scale = 1.0f;
+};
+
 struct MaterialData {
     std::string name;
 
@@ -32,6 +54,26 @@ struct MaterialData {
     AlphaMode alpha_mode = AlphaMode::Opaque;
     float alpha_cutoff = 0.5f;
     bool double_sided = false;
+    bool unlit = false;
+
+    float normal_scale = 1.0f;
+    float specular = 1.0f;
+    Vec3 specular_color {1.0f, 1.0f, 1.0f};
+    float transmission = 0.0f;
+    float thickness = 0.0f;
+    float attenuation_distance = 0.0f;
+    Vec3 attenuation_color {1.0f, 1.0f, 1.0f};
+    Vec3 sheen_color {0.0f, 0.0f, 0.0f};
+    float sheen_roughness = 0.0f;
+    float anisotropy_strength = 0.0f;
+    float anisotropy_rotation = 0.0f;
+    float iridescence = 0.0f;
+    float iridescence_ior = 1.3f;
+    float iridescence_thickness_min = 100.0f;
+    float iridescence_thickness_max = 400.0f;
+    float dispersion = 0.0f;
+    float diffuse_transmission = 0.0f;
+    Vec3 diffuse_transmission_color {1.0f, 1.0f, 1.0f};
 
     std::string texture_path;
     std::string normal_texture_path;
@@ -48,6 +90,29 @@ struct MaterialData {
     TextureHandle ambient_occlusion_texture = INVALID_TEXTURE;
     TextureHandle emissive_texture = INVALID_TEXTURE;
     TextureHandle opacity_texture = INVALID_TEXTURE;
+
+    TextureInfo base_color_info{};
+    TextureInfo metallic_roughness_info{};
+    TextureInfo normal_info{};
+    TextureInfo occlusion_info{};
+    TextureInfo emissive_info{};
+    TextureInfo clearcoat_info{};
+    TextureInfo clearcoat_roughness_info{};
+    TextureInfo clearcoat_normal_info{};
+    TextureInfo sheen_color_info{};
+    TextureInfo sheen_roughness_info{};
+    TextureInfo transmission_info{};
+    TextureInfo thickness_info{};
+    TextureInfo specular_info{};
+    TextureInfo specular_color_info{};
+    TextureInfo iridescence_info{};
+    TextureInfo iridescence_thickness_info{};
+    TextureInfo anisotropy_info{};
+    TextureInfo diffuse_transmission_info{};
+    TextureInfo diffuse_transmission_color_info{};
+
+    std::string extras_json;
+    std::unordered_map<std::string, std::string> extensions_json;
 };
 
 using MaterialMap = std::unordered_map<std::string, MaterialData>;
