11d42b47edf60b3c5be15e9371f9d08fda8bd1a4
xt9y <felix@xt9y.de>
2026-09-11 10:44:15 +0200

    models: define full gltf asset context

---
+++ Sources/Models/Formats/GltfAssets.hpp
@@ -0,0 +1,30 @@
+#ifndef HORSE_MODELS_FORMATS_GLTF_ASSETS_HPP
+#define HORSE_MODELS_FORMATS_GLTF_ASSETS_HPP
+
+#include "Models/Core/Material.hpp"
+#include "Models/Formats/GltfData.hpp"
+
+#include <string>
+#include <vector>
+
+namespace Models::Formats::GltfAssets {
+
+struct TextureRecord {
+    int source = -1;
+    int sampler = -1;
+};
+
+struct Context {
+    GltfData::Context data;
+    std::vector<SamplerData> samplers;
+    std::vector<TextureRecord> textures;
+    std::vector<TextureHandle> image_cache;
+    std::vector<MaterialData> materials;
+};
+
+bool load(Context *context, std::string *error = nullptr);
+bool textureInfo(Context *context, const GltfJson::Value *value, TextureInfo *out, std::string *error = nullptr);
+
+} // namespace Models::Formats::GltfAssets
+
+#endif
