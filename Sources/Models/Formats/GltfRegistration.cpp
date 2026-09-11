5dcd6b23e298bc1b1186d837ea1b2bd75afcd2e3
xt9y <felix@xt9y.de>
2026-09-11 13:51:19 +0200

    models: route gltf through compression preprocessing

--- Sources/Models/Formats/GltfRegistration.cpp
+++ Sources/Models/Formats/GltfRegistration.cpp
@@ -1,6 +1,6 @@
 #include "Models/Formats/GltfAlpha.hpp"
 #include "Models/Formats/GltfAugment.hpp"
-#include "Models/Formats/GltfFull.hpp"
+#include "Models/Formats/GltfDraco.hpp"
 #include "Models/Formats/Registry.hpp"
 
 #include <string>
@@ -12,7 +12,7 @@ bool loadGltf(const std::string& path, Document *output, std::string *error)
 {
     std::string local_error;
     std::string *load_error = error ? error : &local_error;
-    bool loaded = GltfFull::load(path, output, load_error);
+    bool loaded = GltfDraco::load(path, output, load_error);
     if (!loaded && load_error->starts_with("glTF contains no mesh primitives:")) {
         load_error->clear();
         loaded = true;
