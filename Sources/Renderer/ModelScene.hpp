6bf4734f0c1286a8b390bc4d73921348d14ce995
xt9y <felix@xt9y.de>
2026-09-12 02:54:44 +0200

    renderer: add generic model scene bridge

---
+++ Sources/Renderer/ModelScene.hpp
@@ -0,0 +1,61 @@
+#ifndef HORSE_RENDERER_MODEL_SCENE_HPP
+#define HORSE_RENDERER_MODEL_SCENE_HPP
+
+#include "Ecs/Ecs.hpp"
+#include "Models/Models.hpp"
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace Renderer::ModelScene {
+
+struct NodeBinding {
+    std::uint32_t node = Models::INVALID_INDEX;
+    Ecs::Entity entity = Ecs::INVALID_ENTITY;
+    std::vector<Ecs::Entity> parts;
+};
+
+struct Instance {
+    Models::ModelHandle model = Models::INVALID_MODEL;
+    std::uint32_t scene = Models::INVALID_INDEX;
+    std::uint32_t variant = Models::INVALID_INDEX;
+    std::vector<NodeBinding> nodes;
+    std::vector<Ecs::Entity> loose_parts;
+};
+
+struct Options {
+    std::uint32_t scene = Models::INVALID_INDEX;
+    std::uint32_t variant = Models::INVALID_INDEX;
+    bool instantiate_cameras = true;
+    bool instantiate_lights = true;
+    bool activate_first_camera = false;
+};
+
+bool instantiate(
+    Ecs::World& world,
+    Models::ModelHandle model,
+    Instance *output,
+    const Options& options = {},
+    std::string *error = nullptr
+);
+
+bool applyPose(
+    Ecs::World& world,
+    Instance& instance,
+    const Models::Runtime::Pose& pose,
+    std::string *error = nullptr
+);
+
+bool setVariant(
+    Ecs::World& world,
+    Instance& instance,
+    std::uint32_t variant,
+    std::string *error = nullptr
+);
+
+void destroy(Ecs::World& world, Instance& instance);
+
+} // namespace Renderer::ModelScene
+
+#endif
