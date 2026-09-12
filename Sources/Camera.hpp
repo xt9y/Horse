892b01fcfce6fe0ac7fb46b80d9e98e22aabcb20
xt9y <felix@xt9y.de>
2026-09-12 02:57:47 +0200

    camera: retain full asset projection data

--- Sources/Camera.hpp
+++ Sources/Camera.hpp
@@ -7,10 +7,20 @@
 
 namespace Camera {
 
+enum class Projection {
+    Perspective,
+    Orthographic,
+};
+
 struct CameraComponent {
     float fov_degrees = 0.0f;
     float near_plane = 0.0f;
     bool active = false;
+    Projection projection = Projection::Perspective;
+    float far_plane = 0.0f;
+    float aspect_ratio = 0.0f;
+    float xmag = 1.0f;
+    float ymag = 1.0f;
 };
 
 Ecs::Entity activeCamera(const Ecs::World& world);
