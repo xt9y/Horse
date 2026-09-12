3d349b0ce7678adde7f62f598eaffb07be66e0bf
xt9y <felix@xt9y.de>
2026-09-12 11:35:46 +0200

    gaussian: dispatch Metal compositor

--- Sources/Renderer/GaussianSplat/GaussianSplat.cpp
+++ Sources/Renderer/GaussianSplat/GaussianSplat.cpp
@@ -1,6 +1,9 @@
 #include "Renderer/GaussianSplat/GaussianSplat.hpp"
 
 #include "Renderer/GaussianSplat/OpenGL/GaussianSplatOpenGL.hpp"
+#ifdef __APPLE__
+#include "Renderer/GaussianSplat/Metal/GaussianSplatMetal.hpp"
+#endif
 
 namespace Renderer::GaussianSplat {
 
@@ -8,12 +11,19 @@ bool render(const Ecs::World& world, Internal::FrameOutput& output)
 {
     if (output.api == Internal::GraphicsApi::OpenGL)
         return OpenGL::render(world, output);
+#ifdef __APPLE__
+    if (output.api == Internal::GraphicsApi::Metal)
+        return Metal::render(world, output);
+#endif
     return true;
 }
 
 void shutdown()
 {
     OpenGL::shutdown();
+#ifdef __APPLE__
+    Metal::shutdown();
+#endif
 }
 
 } // namespace Renderer::GaussianSplat
