4368ea886992d9deb203cf25b4406ec4cfc618a4
xt9y <felix@xt9y.de>
2026-09-12 11:58:16 +0200

    trace: enable advanced materials in Metal ray tracer

--- Sources/Renderer/RayTracer/Metal/RayTracerMetalShaders.hpp
+++ Sources/Renderer/RayTracer/Metal/RayTracerMetalShaders.hpp
@@ -2,11 +2,13 @@
 #define RW_ENGINE_RENDERER_RAYTRACER_METAL_SHADERS_HPP
 
 #include "Renderer/PathTracer/Metal/PbrMetalShaders.hpp"
+#include "Renderer/Trace/Metal/AdvancedMaterialShaders.hpp"
 #include "Renderer/Trace/Metal/CameraProjectionShaders.hpp"
 
 namespace Renderer::RayTracerMetalShaders {
-inline const std::string source_storage =
-    Trace::Metal::cameraProjectionShaderSource(PbrMetalShaders::source);
+inline const std::string source_storage = Renderer::Trace::Metal::advancedMaterialShaderSource(
+    Renderer::Trace::Metal::cameraProjectionShaderSource(Renderer::PbrMetalShaders::source)
+);
 inline const char *source = source_storage.c_str();
 }
 
