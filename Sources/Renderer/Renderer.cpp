8a8a65504e01866e377fbf2ccb8a75ec06533aff
xt9y <felix@xt9y.de>
2026-09-12 11:18:57 +0200

    renderer: composite Gaussian splats before presentation

--- Sources/Renderer/Renderer.cpp
+++ Sources/Renderer/Renderer.cpp
@@ -2,6 +2,7 @@
 
 #include "Renderer/Debug/Internal.hpp"
 #include "Renderer/Fonts/FontPass.hpp"
+#include "Renderer/GaussianSplat/GaussianSplat.hpp"
 #include "Renderer/GlobalIllumination/GlobalIllumination.hpp"
 #include "Renderer/ShadingState.hpp"
 #include "UI/Internal.hpp"
@@ -17,6 +18,7 @@ void IRenderer::render(const Ecs::World& world)
     output.global_illumination = GlobalIllumination::update(world);
     if (!renderScene(world, output)) return;
     if (post_process_ && !post_process_->process(output)) return;
+    if (!GaussianSplat::render(world, output)) return;
     if (!compose(output)) return;
     Debug::Internal::render(world, output);
     Internal::renderFonts(world, output);
