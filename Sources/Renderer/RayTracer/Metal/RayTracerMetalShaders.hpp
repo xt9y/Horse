91b2914e444b07f8024ee8c16340fbf3218a96d9
xt9y <felix@xt9y.de>
2026-09-12 11:59:02 +0200

    trace: finalize Metal ray material shader composition

--- Sources/Renderer/RayTracer/Metal/RayTracerMetalShaders.hpp
+++ Sources/Renderer/RayTracer/Metal/RayTracerMetalShaders.hpp
@@ -2,11 +2,11 @@
 #define RW_ENGINE_RENDERER_RAYTRACER_METAL_SHADERS_HPP
 
 #include "Renderer/PathTracer/Metal/PbrMetalShaders.hpp"
-#include "Renderer/Trace/Metal/AdvancedMaterialShaders.hpp"
+#include "Renderer/Trace/Metal/AdvancedMaterialShaderFixes.hpp"
 #include "Renderer/Trace/Metal/CameraProjectionShaders.hpp"
 
 namespace Renderer::RayTracerMetalShaders {
-inline const std::string source_storage = Renderer::Trace::Metal::advancedMaterialShaderSource(
+inline const std::string source_storage = Renderer::Trace::Metal::finalizedAdvancedMaterialShaderSource(
     Renderer::Trace::Metal::cameraProjectionShaderSource(Renderer::PbrMetalShaders::source)
 );
 inline const char *source = source_storage.c_str();
