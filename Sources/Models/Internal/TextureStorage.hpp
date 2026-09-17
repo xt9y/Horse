016bb1bf7f6263eeac90e63d74e090cd676be7b0
xt9y <felix@xt9y.de>
2026-09-17 14:38:10 +0200

    Expose internal texture storage count

--- Sources/Models/Internal/TextureStorage.hpp
+++ Sources/Models/Internal/TextureStorage.hpp
@@ -3,6 +3,7 @@
 
 #include "Models/Core/Texture.hpp"
 
+#include <cstddef>
 #include <string>
 
 namespace Models::Internal {
@@ -10,6 +11,7 @@ namespace Models::Internal {
 std::string normalizeTexturePath(const std::string& path);
 TextureHandle findTexture(const std::string& key);
 TextureHandle reserveTexture(const std::string& key);
+std::size_t textureStorageCount();
 bool textureStorageReady(TextureHandle handle);
 bool publishTexture(TextureHandle handle, Images::Image image);
 
