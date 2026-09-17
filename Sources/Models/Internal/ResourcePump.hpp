4d870062fd6da33fbd75f7dbd441f30c2525d6ac
xt9y <felix@xt9y.de>
2026-09-18 00:45:57 +0200

    Add shared model resource pump interface

---
+++ Sources/Models/Internal/ResourcePump.hpp
@@ -0,0 +1,12 @@
+#ifndef HORSE_MODELS_INTERNAL_RESOURCE_PUMP_HPP
+#define HORSE_MODELS_INTERNAL_RESOURCE_PUMP_HPP
+
+#include <cstddef>
+
+namespace Models::Internal {
+
+std::size_t pumpResources();
+
+} // namespace Models::Internal
+
+#endif
