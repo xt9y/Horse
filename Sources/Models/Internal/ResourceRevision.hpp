e095fd054d2dc7387960bfd96008e04542ac0f32
xt9y <felix@xt9y.de>
2026-09-17 12:29:57 +0200

    Expose model resource revision hook

---
+++ Sources/Models/Internal/ResourceRevision.hpp
@@ -0,0 +1,10 @@
+#ifndef HORSE_MODELS_INTERNAL_RESOURCE_REVISION_HPP
+#define HORSE_MODELS_INTERNAL_RESOURCE_REVISION_HPP
+
+namespace Models::Internal {
+
+void touchResources();
+
+} // namespace Models::Internal
+
+#endif
