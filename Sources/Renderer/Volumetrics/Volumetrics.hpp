5b4595962ac958420ce92fa4b7ad591ab9de9d43
xt9y <felix@xt9y.de>
2026-09-18 16:40:11 +0200

    Add volumetric quality presets

--- Sources/Renderer/Volumetrics/Volumetrics.hpp
+++ Sources/Renderer/Volumetrics/Volumetrics.hpp
@@ -1,11 +1,14 @@
 #ifndef HORSE_RENDERER_VOLUMETRICS_VOLUMETRICS_HPP
 #define HORSE_RENDERER_VOLUMETRICS_VOLUMETRICS_HPP
 
+#include "Renderer/Quality.hpp"
+
 #include <cstdint>
 
 namespace Renderer::Volumetrics {
 
 struct Settings {
+    Quality quality = Quality::High;
     bool enabled = true;
     int resolution_divisor = 2;
     std::uint32_t sample_count = 32u;
@@ -19,6 +22,40 @@ struct Settings {
 
 Settings& settings();
 const Settings& currentSettings();
+
+inline void setQuality(Quality value)
+{
+    Settings& current = settings();
+    current.quality = value;
+    switch (value) {
+    case Quality::Low:
+        current.resolution_divisor = 4;
+        current.sample_count = 16u;
+        current.blur_passes = 2u;
+        break;
+    case Quality::Medium:
+        current.resolution_divisor = 3;
+        current.sample_count = 24u;
+        current.blur_passes = 3u;
+        break;
+    case Quality::High:
+        current.resolution_divisor = 2;
+        current.sample_count = 32u;
+        current.blur_passes = 4u;
+        break;
+    case Quality::Ultra:
+        current.resolution_divisor = 1;
+        current.sample_count = 48u;
+        current.blur_passes = 4u;
+        break;
+    }
+}
+
+inline Quality quality()
+{
+    return currentSettings().quality;
+}
+
 void shutdown();
 
 } // namespace Renderer::Volumetrics
