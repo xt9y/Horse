ccaff072da3a24b2bac90d1bf7b0ab31c85ea229
xt9y <felix@xt9y.de>
2026-09-18 13:36:25 +0200

    Own Gaussian splat settings

--- Sources/Renderer/GaussianSplat/GaussianSplat.cpp
+++ Sources/Renderer/GaussianSplat/GaussianSplat.cpp
@@ -3,9 +3,25 @@
 #include "Renderer/GaussianSplat/GaussianSplatSDLGPU.hpp"
 
 namespace Renderer::GaussianSplat {
+namespace {
+
+Settings current_settings;
+
+} // namespace
+
+Settings& settings()
+{
+    return current_settings;
+}
+
+const Settings& currentSettings()
+{
+    return current_settings;
+}
 
 bool render(const Ecs::World& world, Internal::FrameOutput& output)
 {
+    if (!current_settings.enabled) return true;
     if (output.api == Internal::GraphicsApi::SDLGPU)
         return SDLGPU::render(world, output);
     return true;
