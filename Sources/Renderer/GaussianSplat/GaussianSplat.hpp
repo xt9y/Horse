353ca97604740e7825579ed05c899f50bf1fc729
xt9y <felix@xt9y.de>
2026-09-18 13:36:17 +0200

    Expose Gaussian splat settings

--- Sources/Renderer/GaussianSplat/GaussianSplat.hpp
+++ Sources/Renderer/GaussianSplat/GaussianSplat.hpp
@@ -6,6 +6,16 @@
 
 namespace Renderer::GaussianSplat {
 
+struct Settings {
+    bool enabled = true;
+    float radius = 3.0f;
+    float minimum_depth_epsilon = 0.0025f;
+    float relative_depth_epsilon = 0.0005f;
+};
+
+Settings& settings();
+const Settings& currentSettings();
+
 bool render(const Ecs::World& world, Internal::FrameOutput& output);
 void shutdown();
 
