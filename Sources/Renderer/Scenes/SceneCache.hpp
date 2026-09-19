8eb476fd1b74cdd30be0e0de09a37d16fd94c58d
xt9y <felix@xt9y.de>
2026-09-19 14:40:53 +0200

    Use measured eight-triangle BVH leaves

--- Sources/Renderer/Scenes/SceneCache.hpp
+++ Sources/Renderer/Scenes/SceneCache.hpp
@@ -126,7 +126,7 @@ private:
     void clearGeometry();
     void clearResources();
 
-    inline static std::uint32_t leaf_size_ = 0u;
+    inline static std::uint32_t leaf_size_ = 8u;
     inline static std::size_t maximum_triangles_ = std::numeric_limits<std::size_t>::max();
     inline static float opacity_cutoff_ = 0.0f;
     inline static std::uint8_t alpha_threshold_ = 0u;
