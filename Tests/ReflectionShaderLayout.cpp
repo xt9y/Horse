a454f4b11610ab6c2641fe67f8bd2862a4fdf456
xt9y <felix@xt9y.de>
2026-09-19 10:37:28 +0200

    Require prefiltered environment atlas shading

--- Tests/ReflectionShaderLayout.cpp
+++ Tests/ReflectionShaderLayout.cpp
@@ -14,6 +14,9 @@ int main()
     assert(shader.find("Texture2DArray<float> ShadowMaps : register(t15, space2)") != std::string_view::npos);
     assert(shader.find("StructuredBuffer<GpuBaseMaterial> PBaseMaterials : register(t16, space2)") != std::string_view::npos);
     assert(shader.find("StructuredBuffer<float4> PReflectionProbes : register(t23, space2)") != std::string_view::npos);
+    assert(shader.find("PrefilteredEnvironmentColor") != std::string_view::npos);
+    assert(shader.find("float3(u, v, 0.0)") != std::string_view::npos);
+    assert(shader.find("PReflectionProbes[0].z") != std::string_view::npos);
     assert(shader.find("SampleReflectionProbe") != std::string_view::npos);
     assert(shader.find("BoxProjectedDirection") != std::string_view::npos);
     assert(shader.find("LocalReflectionColor") != std::string_view::npos);
