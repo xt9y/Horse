228f3bbb0104ad2a3561273331b6691053e6f8d6
xt9y <felix@xt9y.de>
2026-09-19 11:12:34 +0200

    Register indexed raster submission test

--- build.c
+++ build.c
@@ -127,6 +127,10 @@ void build(C_Build *b)
     c_sources(raster_topology_tests, "Tests/RasterTopologyRevision.cpp");
     configureTest(raster_topology_tests, library);
 
+    C_Target *raster_submission_tests = c_test(b, "HorseRasterIndexedSubmissionTests");
+    c_sources(raster_submission_tests, "Tests/RasterIndexedSubmission.cpp");
+    configureTest(raster_submission_tests, library);
+
     C_Target *staged_model_tests = c_test(b, "HorseStagedModelTests");
     c_sources(staged_model_tests, "Tests/StagedModel.cpp");
     configureTest(staged_model_tests, library);
