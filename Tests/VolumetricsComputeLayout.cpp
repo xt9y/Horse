c0be4ea8fc722a0a6e8fba205b37f4f35a9ee1ed
xt9y <felix@xt9y.de>
2026-09-19 15:52:06 +0200

    Require TLAS BLAS volumetric compute layout

--- Tests/VolumetricsComputeLayout.cpp
+++ Tests/VolumetricsComputeLayout.cpp
@@ -24,12 +24,21 @@ int main()
 
     assert(march.find("Texture2D<float> SceneDepth : register(t0, space0)") != std::string::npos);
     assert(march.find("SamplerState DepthSampler : register(s0, space0)") != std::string::npos);
-    assert(march.find("StructuredBuffer<GpuNode> Nodes : register(t1, space0)") != std::string::npos);
-    assert(march.find("StructuredBuffer<GpuTriangle> Triangles : register(t2, space0)") != std::string::npos);
-    assert(march.find("StructuredBuffer<float4> Shading : register(t3, space0)") != std::string::npos);
+    assert(march.find("StructuredBuffer<GpuNode> TlasNodes : register(t1, space0)") != std::string::npos);
+    assert(march.find("StructuredBuffer<GpuInstance> Instances : register(t2, space0)") != std::string::npos);
+    assert(march.find("StructuredBuffer<GpuNode> BlasNodes : register(t3, space0)") != std::string::npos);
+    assert(march.find("StructuredBuffer<uint4> Blases : register(t4, space0)") != std::string::npos);
+    assert(march.find("StructuredBuffer<GpuTriangle> LocalTriangles : register(t5, space0)") != std::string::npos);
+    assert(march.find("StructuredBuffer<GpuNode> DynamicNodes : register(t6, space0)") != std::string::npos);
+    assert(march.find("StructuredBuffer<GpuTriangle> DynamicTriangles : register(t7, space0)") != std::string::npos);
+    assert(march.find("StructuredBuffer<float4> Shading : register(t8, space0)") != std::string::npos);
     assert(march.find("RWTexture2D<float4> Output : register(u0, space1)") != std::string::npos);
     assert(march.find("cbuffer FrameData : register(b0, space2)") != std::string::npos);
     assert(march.find("cbuffer VolumetricData : register(b1, space2)") != std::string::npos);
+    assert(march.find("cbuffer AccelerationData : register(b2, space2)") != std::string::npos);
+    assert(march.find("OccludedTlas") != std::string::npos);
+    assert(march.find("OccludedBlas") != std::string::npos);
+    assert(march.find("OccludedDynamic") != std::string::npos);
     assert(march.find("space3") == std::string::npos);
 
     assert(text.find("bool pipelines_attempted = false;") != std::string::npos);
